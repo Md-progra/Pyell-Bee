dbac0685b32aafc193aae40940c558f6faa30e4f
Angel  Sarpong Antwi-Mensah <antwiangel77@gmail.com>
2024-12-05 23:06:20 +0000

    Creation of two new functions in program.py

--- program.py
+++ program.py
@@ -19,3 +19,31 @@ from tkinter import messagebox
                 print("Sorry, I couldn't understand what you said")
                 return "Sorry, I couldn't understand what you said"
                 messagebox.showerror("Error", "Sorry I couldn't understand what you said")
+    # Creating a function to spell back to user
+    def spell_back_in_audio(text):
+
+        engine = pyttsx3.init() #initializing the engine
+        engine.setProperty('rate', 150) #setting the rate of speech
+        words = text.split() #splitting the text into letters
+
+        for word in words: #looping through the words to spell them back
+            for letter in word: # looping through the letters 
+            engine.say(letter): 
+            engine.runAndWait() #running the engine
+            time.sleep(1)  #pauses for a second
+
+            # engine.say(" ") #pauses for a second
+            # engine.runAndWait()
+    # Creating the listeening function
+    def start_to_listen():
+        tex = listen_convert_to_text()
+        if text:
+            spell_back_in_audio(text)
+  
+  # Creation of GUI
+  root = tk.Tk() #Creating thr root window
+  root.title("Pyell Bee") #Setting title of program
+
+  frame = tk.Frame(root,bg = "lightblue",padx = 10,pady=10,bd = 5, relief = tk.RAISED) #Creating and setting/designing the frame
+
+
